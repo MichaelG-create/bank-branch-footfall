e7199629e6ab8e434a4a87fc70ea6dd19845e2cc
Michael-pc <michael.garcia.73@gmail.com>
2024-12-30 22:58:57 +0100

    added test_sensor.py with 5 tests on get_visitor_count

--- src/sensor.py
+++ src/sensor.py
@@ -39,8 +39,10 @@ class VisitorCounter:
         """
         hour_visits = None
 
-        # for the random generations -> concat y,m,d (no hours, we want the same result whatever the hours)
-        formated_date = int(dt.strftime("%Y%m%d")) # %H if we want to have the bad event happening on an hour basis
+        # for random generations -> concat y,m,d (not hours, want same result whatever the hours)
+        formated_date = int(
+            dt.strftime("%Y%m%d")
+        )  # %H if we want to have the bad event happening on an hour basis
 
         try:
             # if the counter works
@@ -53,16 +55,21 @@ class VisitorCounter:
                 )
                 # modulate visits with week_day
                 day_avg_visits = self.get_day_visits(random_visit_count, dt)
-                # modulate visits with hour of the day
-                hour_visits = self.get_hour_visits(day_avg_visits, dt)
+
+                # if we're closed, no need to modulate hour
+                if day_avg_visits == -1:
+                    hour_visits = -1
+                else:
+                    # modulate visits with hour of the day
+                    hour_visits = self.get_hour_visits(day_avg_visits, dt)
 
                 # works bad : only 20% traffic counted
-                if self.is_badly_working(formated_date):
-                    print('bad counting')
-                    hour_visits = int(0.2*hour_visits)
+                if self.is_badly_counting(formated_date):
+                    print("bad counting")
+                    hour_visits = int(0.2 * hour_visits)
             # defective counter : sends -1
             else:
-                hour_visits = -1
+                hour_visits = -10
                 # print('dead counter')
 
         except ValueError as e:
@@ -71,14 +78,14 @@ class VisitorCounter:
         return hour_visits
 
     @staticmethod
-    def is_badly_working(seed_date: int) -> bool:
+    def is_badly_counting(seed_date: int) -> bool:
         """
         random number to see if counter works badly (count only a fraction of the traffic)
         :param seed_date:
         :return: True or False
         """
-        np.random.seed(seed_date) # reproducibility
-        bad_behavior_rate = 0.005 # greater than 0.0015 test already passed
+        np.random.seed(seed_date)  # reproducibility
+        bad_behavior_rate = 0.005  # greater than 0.0015 test already passed
         return np.random.random() <= bad_behavior_rate
 
     @staticmethod
@@ -88,7 +95,7 @@ class VisitorCounter:
         :param seed_date:
         :return: True or False
         """
-        np.random.seed(seed_date) # reproducibility
+        np.random.seed(seed_date)  # reproducibility
         malfunction_rate = 0.002
         return np.random.random() > malfunction_rate
 
@@ -97,22 +104,23 @@ class VisitorCounter:
         """
         Rules for the hours:
         --------------------
-        00:00-09:00 : traffic_fraction = 0
+        00:00-09:00 : traffic_fraction = -1
         09:00-10:00 : traffic_fraction = 20 % * day_avg_visits
         10:00-11:00 : traffic_fraction = 25 % * day_avg_visits
         11:00-12:00 : traffic_fraction = 25 % * day_avg_visits
-        12:00-13:00 : traffic_fraction = 0
+        12:00-13:00 : traffic_fraction = -1
         13:00-14:00 : traffic_fraction = 10 % * day_avg_visits
         14:00-15:00 : traffic_fraction = 10 % * day_avg_visits
         15:00-16:00 : traffic_fraction =  2 % * day_avg_visits
         16:00-17:00 : traffic_fraction =  3 % * day_avg_visits
         17:00-18:00 : traffic_fraction =  5 % * day_avg_visits
-        18:00-00:00 : traffic_fraction = 0
+        18:00-00:00 : traffic_fraction = -1
 
         :param day_avg_visits: visits of the current day
         :param dt: datetime
         :return:
         """
+
         if dt.hour == 9:
             hour_visits = int(day_avg_visits * 0.2)
         elif dt.hour == 10:
@@ -131,7 +139,7 @@ class VisitorCounter:
         elif dt.hour == 17:
             hour_visits = int(day_avg_visits * 0.05)
         else:
-            hour_visits = 0
+            hour_visits = -1
         return hour_visits
 
     @staticmethod
@@ -164,9 +172,9 @@ class VisitorCounter:
         elif dt.weekday() == 4:
             day_avg_visits = random_visit_count * 1.8
         elif dt.weekday() == 5:
-            day_avg_visits = 0
+            day_avg_visits = -1
         elif dt.weekday() == 6:
-            day_avg_visits = 0
+            day_avg_visits = -1
         else:
             raise ValueError("weekday not between 0 and 6!")
         return day_avg_visits
@@ -190,4 +198,3 @@ if __name__ == "__main__":
             f"got {visitors} visitors"
         )
         date += increment
-
