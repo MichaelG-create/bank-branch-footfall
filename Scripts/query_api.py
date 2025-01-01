377de9ced9999ecc3957b032288873ff7259ab81
Michael-pc <michael.garcia.73@gmail.com>
2025-01-01 14:44:48 +0100

    -pylint and black of everything

--- Scripts/query_api.py
+++ Scripts/query_api.py
@@ -1,5 +1,7 @@
+"""
+Module to query the API directly on render.com
+Loop on multiple dates and hours to create analytical reports
+"""
 
-
-
-if __name__ == '__main__':
-    print('Hello World!')
+if __name__ == "__main__":
+    print("Hello World!")
