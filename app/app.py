ff471e3eaf1270f448f279d1e23758358ab3ca84
Michael-pc <michael.garcia.73@gmail.com>
2025-02-01 09:02:22 +0100

    -display chosen sensor dataframe in streamlit

--- app/app.py
+++ app/app.py
@@ -3,32 +3,68 @@
 import duckdb
 import streamlit as st
 
-# Read the parquet file to df
-PARQUET_FILE = (
-    "'data/filtered/2024_agencies_daily_visitor_count/"
-    "part-00000-0e27bd92-4b26-465a-9cf9-180b61966469-c000.snappy.parquet'"
-)
 
-# PARQUET_FILE = "'app/1.parquet'"
+def get_sensor_list(parquet_file: str) -> list[tuple[str, int]]:
+    """reads the parquet file and returns the list of (agencies, sensors)"""
+    # pylint: disable=C0303
+    query = f"""
+            WITH agency_sensors_cte AS (  
+              SELECT DISTINCT agency_name, counter_id 
+              FROM {parquet_file} 
+              -- LIMIT 1000
+              ORDER BY agency_name, counter_id)
+            
+            SELECT agency_name, counter_id
+            FROM agency_sensors_cte;
+            """
+    # duckdb.sql(query).show()
+    agency_sensor = duckdb.sql(query).fetchall()
+    return agency_sensor
 
-# pylint: disable=C0303
-QUERY = f"""
-WITH agency_sensors_cte AS (  
-  SELECT DISTINCT agency_name, counter_id 
-  FROM {PARQUET_FILE} 
-  -- LIMIT 1000
-  ORDER BY agency_name, counter_id)
 
-SELECT agency_name || ' - counter #' || counter_id AS agency_counter
-FROM agency_sensors_cte;
-"""
+def get_sensor_chosen(agency_sensor_lst: list[tuple[str, int]]) -> tuple[str, int]:
+    """from the list agency_sensor_lst, display a selectbox
+    returns the tuple (agency_name, counter_id) chosen by the user"""
+    agency_sensor_list_str = [
+        f"Agence {row[0]} - compteur # {row[1]}" for row in agency_sensor_lst
+    ]
 
-# duckdb.sql(QUERY).show()
-sensor_list = duckdb.sql(QUERY).fetchall()
-sensor_list = [row[0] for row in sensor_list]
-print(sensor_list)
-print(type(sensor_list))
+    sensor_chosen = st.selectbox("Choisir un capteur", agency_sensor_list_str)
 
-# Display a list of all sensors
-# with st.sidebar:
-st.selectbox("Choisir un capteur", sensor_list)
+    agency_sensor_dict = dict(zip(agency_sensor_list_str, agency_sensor_lst))
+
+    return agency_sensor_dict[sensor_chosen]
+
+
+def display_sensor_dataframe(agency_n: str, counter_i: int, parquet_file: str):
+    """displays the dataframe of the chosen sensor"""
+    # pylint: disable=C0303
+    query = f"""
+              SELECT * 
+              FROM {parquet_file} 
+              WHERE agency_name = '{agency_n}' and counter_id = {counter_i}::INTEGER
+              ORDER BY agency_name, counter_id, date;
+            """
+    # duckdb.sql(query).show()
+    print(query)
+    sensor_df = duckdb.sql(query).df()
+    print(sensor_df)
+    st.dataframe(sensor_df)
+
+
+if __name__ == "__main__":
+
+    # Read the parquet file directly in duckdb (memory costless)
+    PARQUET_FILE = (
+        "'data/filtered/2024_agencies_daily_visitor_count/"
+        "part-00000-0e27bd92-4b26-465a-9cf9-180b61966469-c000.snappy.parquet'"
+    )
+
+    agency_sensor_list = get_sensor_list(PARQUET_FILE)
+
+    # Display a list of all sensors to be chosen
+    # find the corresponding sensor agency_name and counter_id
+    agency, sensor = get_sensor_chosen(agency_sensor_list)
+
+    print(agency, sensor, PARQUET_FILE)
+    display_sensor_dataframe(agency, sensor, PARQUET_FILE)
