713f9290012fba5f9e773229ca1fc42c0883efcb
Michael-pc <michael.garcia.73@gmail.com>
2025-02-18 22:15:34 +0100

    -save live adress in comments in api/app.py

--- api/app.py
+++ api/app.py
@@ -4,7 +4,13 @@ using the class VisitorCounter
 and its method get_visitor_count
 at a certain date
 e.g. :
-/get_visitor_count?date_time=2025-05-29_09:05
+
+web version:
+https://bank-branch-footfall.onrender.com/
+get_visitor_count?date_time=2025-05-29%2009:05&agency_name=Aix_les_bains_1
+
+local version:
+
 http://127.0.0.1:8000/get_visitor_count?date_time=2025-05-29%2009:05&agency_name=Aix_les_bains_1
 TO LAUNCH it locally :
 in this project folder, with venv activated :
